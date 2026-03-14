9188677e19cee0637300ef9d6e543f6c51dc68ac
0xMikey-ooze <119510170+0xMikey-ooze@users.noreply.github.com>
2026-03-14 12:15:26 -0400

    chore: deprecate stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,5 +1,1 @@
-import Stripe from "stripe";
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16" as any,
-});
+// This file is deprecated. Payments now use Polar.sh (see src/lib/polar.ts)
